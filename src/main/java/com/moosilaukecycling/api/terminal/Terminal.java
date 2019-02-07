6ee3e59ff54f9e1c9de3b891a841f6df2c520200
Sam Wright <sam.wri8@gmail.com>
2019-02-07 05:05:37 -0800

    simple terminal for quick use

--- src/main/java/com/moosilaukecycling/api/terminal/Terminal.java
+++ src/main/java/com/moosilaukecycling/api/terminal/Terminal.java
@@ -1,16 +1,85 @@
 package com.moosilaukecycling.api.terminal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Terminal {
 
-    public Terminal() {
+    private static final String EXIT = "q";
 
+    BufferedReader bufferedReader;
+
+    public Terminal() {
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
+    // 1. create repair job
+    // 2. checkout
+    // 3. list inventory
+    // 4. add to inventory
     public void run() {
-        // 1. create repair job
-        // 2. checkout completed repair job
-        // 3. list inventory
-        // 4. add to inventory
-        // 5. purchase item
+        System.out.println("Enter choice, or '" + EXIT + "' to quit.");
+
+        while (true) {
+
+            System.out.print("> ");
+            String input = "";
+            try {
+                input = bufferedReader.readLine();
+            } catch (IOException e) {
+                System.err.println("Something went wrong. Quiting.");
+                e.printStackTrace();
+                break;
+            }
+
+            if (input.toLowerCase().equals(EXIT)) {
+                System.out.println("Quiting.");
+                break;
+            }
+            switch (input) {
+                case "1":
+                    createRepairJob();
+                    break;
+                case "2":
+                    checkout();
+                    break;
+                case "3":
+                    listInventory();
+                    break;
+                case "4":
+                    addInventory();
+                    break;
+                default:
+                    System.out.println("Unknown choice. Enter '" + EXIT + "' to quit.");
+
+            }
+        }
+    }
+
+    //TODO
+
+    private void createRepairJob() {
+        System.out.println("Creating repair job.");
+    }
+
+    private void checkout() {
+        System.out.println("Checking out.");
+    }
+
+    private void listInventory() {
+        System.out.println("Listing inventory.");
+    }
+
+    private void addInventory() {
+        System.out.println("Adding inventory.");
+    }
+
+    public void close() {
+        try {
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
